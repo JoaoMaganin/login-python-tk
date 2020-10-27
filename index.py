db2b6b89c6ad061d36966c7470c40fb44babe947
JoaoMaganin <joaovitormontemor@gmail.com>
2020-10-27 08:58:12 -0300

    Criação da página principal com os frames e pasta images

---
+++ index.py
@@ -0,0 +1,18 @@
+from tkinter import *
+from tkinter import ttk
+
+screen = Tk()
+screen.geometry("500x600")
+screen.title("Login com Python")
+screen.configure(bg="white")
+screen.resizable(0, 0)
+
+top_frame = Frame(screen, width=493, height=200, bg="#7ed957")
+top_frame.pack(side=TOP)
+
+bot_frame = Frame(screen, width=493, height=395, bg="#7ed957")
+bot_frame.pack(side=BOTTOM)
+
+logo
+
+screen.mainloop()
